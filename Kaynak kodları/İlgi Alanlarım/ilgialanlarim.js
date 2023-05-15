3a7160a679ba8d44a877e04a9772ce8a60c4fc0c
MuhammedYusufOngel <132345139+MuhammedYusufOngel@users.noreply.github.com>
2023-05-15 23:10:29 +0300

    Add files via upload

--- Kaynak kodları/İlgi Alanlarım/ilgialanlarim.js
+++ Kaynak kodları/İlgi Alanlarım/ilgialanlarim.js
@@ -1,18 +1,27 @@
-const url = 'https://ergast.com/api/f1/'
+const url = 'https://ergast.com/api/f1/2013/driverstandings.json'
 
+let pos = document.querySelector('.pos')
+let driver = document.querySelector('.driver')
+let skor = document.querySelector('.point')
+let constructor = document.querySelector('.constructor')
 
-document.getElementById("ilgiAlanlarim").onclick = function () {
-    let query = `https://ergast.com/api/f1/2013/driverstandings.json`
-    fetch(query)
-    .then(result => {
-        return result.json()
-    })
-    .then (displayResult)
+const displayStandings = (standings) => {
+
+    console.log(standings)
+
+    for (i = 0; i < 23; i++) {
+        driver.innerHTML += `${standings.MRData.StandingsTable.StandingsLists.at(0).DriverStandings.at(i).Driver.givenName} ${standings.MRData.StandingsTable.StandingsLists.at(0).DriverStandings.at(i).Driver.familyName}<br>`
+        constructor.innerHTML += `${standings.MRData.StandingsTable.StandingsLists.at(0).DriverStandings.at(i).Constructors.at(0).name}<br>`
+        skor.innerHTML += `${standings.MRData.StandingsTable.StandingsLists.at(0).DriverStandings.at(i).points}<br>`
+    }
+
 }
 
-const displayResult = (result) => {
-    console.log(result)
-    let driver = document.querySelector('.driver')
-    driver.innerHTML = `${result.MRData.StandingsTable.StandingsLists.at(0).DriverStandings.at(0).Driver.familyName}, ${result.MRData.StandingsTable.StandingsLists.at(0).DriverStandings.at(0).Driver.givenName}`
+let query = url
+fetch(query)
+.then(result => {
+    return result.json()          
+})
+.then(displayStandings)
 
-}
+
